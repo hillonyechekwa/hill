5563bbe27b6c4f94e599ba40aba008b65d30cfef
Hill onyechekwa <hillonyechekwa@gmail.com>
2023-06-14 09:47:15 +0100

    fixed errors and set development server

--- .eleventy.js
+++ .eleventy.js
@@ -6,12 +6,14 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("./src/fonts/Gotham-Light.woff2")
 	eleventyConfig.addPassthroughCopy("./src/fonts/GopherText-Regular.woff2")
 	eleventyConfig.addPassthroughCopy("./src/fonts/GopherText-Bold.woff2")
+    eleventyConfig.addPassthroughCopy("./src/favicons")
     eleventyConfig.addPassthroughCopy("./src/assets")
-    eleventyConfig.addWatchTraget("./src/_includes/css")
+    eleventyConfig.addWatchTarget("./src/_includes/css")
     eleventyConfig.addPassthroughCopy("./src/fonts")
+    eleventyConfig.addPassthroughCopy("./src/resume/");
     eleventyConfig.addPassthroughCopy("./src/scripts")
     eleventyConfig.addPassthroughCopy({ "./src/favicons": "/" })
-    eleventyConfig.addShortCode("year", () => `${new Date().getFullYear()}`)
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`)
 
     return {
         dir: {
